bfdcf4c41d89d66f091c3dce1ed4ecff822b939e
sausagecy <jiangchangyuan999@gmail.com>
2019-06-13 10:56:19 +0800

    add arguments in ArgumentParser()

--- utils/train_segmentation.py
+++ utils/train_segmentation.py
@@ -6,6 +6,14 @@ import torch
 import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
+import sys
+
+repo_name = 'pointnet.pytorch'
+abs_dir = os.path.realpath('.')
+repo_dir = abs_dir[:abs_dir.index(repo_name)+len(repo_name)]
+sys.path.insert(0, repo_dir)
+print(sys.path[0])
+
 from pointnet.dataset import ShapeNetDataset
 from pointnet.model import PointNetDenseCls, feature_transform_regularizer
 import torch.nn.functional as F
@@ -17,13 +25,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     '--batchSize', type=int, default=32, help='input batch size')
 parser.add_argument(
+    '--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument(
+    '--npoints', type=int, default=2500, help='the number of points of every shape')
+parser.add_argument(
     '--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument(
     '--nepoch', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--outf', type=str, default='seg', help='output folder')
 parser.add_argument('--model', type=str, default='', help='model path')
 parser.add_argument('--dataset', type=str, required=True, help="dataset path")
-parser.add_argument('--class_choice', type=str, default='Chair', help="class_choice")
+parser.add_argument('--class_choice', type=str, default=None, help="class_choice")
 parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
 
 opt = parser.parse_args()
@@ -34,27 +46,48 @@ print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
 
+if opt.class_choice is None:
+    cls_choice = None
+else:
+    cls_choice = [opt.class_choice]
+
 dataset = ShapeNetDataset(
     root=opt.dataset,
+    npoints=opt.npoints,
     classification=False,
-    class_choice=[opt.class_choice])
+    class_choice=cls_choice)
 dataloader = torch.utils.data.DataLoader(
     dataset,
     batch_size=opt.batchSize,
     shuffle=True,
     num_workers=int(opt.workers))
 
+val_dataset = ShapeNetDataset(
+    root = opt.dataset,
+    npoints=opt.npoints,
+    classification=False,
+    split='val',
+    class_choice=cls_choice)
+valdataloader = torch.utils.data.DataLoader(
+    val_dataset,
+    batch_size=opt.batchSize,
+    shuffle=True,
+    num_workers=int(opt.workers),
+    drop_last=True)
+
 test_dataset = ShapeNetDataset(
     root=opt.dataset,
+    npoints=opt.npoints,
     classification=False,
-    class_choice=[opt.class_choice],
+    class_choice=cls_choice,
     split='test',
     data_augmentation=False)
 testdataloader = torch.utils.data.DataLoader(
     test_dataset,
     batch_size=opt.batchSize,
     shuffle=True,
-    num_workers=int(opt.workers))
+    num_workers=int(opt.workers),
+    drop_last=True)
 
 print(len(dataset), len(test_dataset))
 num_classes = dataset.num_seg_classes
@@ -98,8 +131,8 @@ for epoch in range(opt.nepoch):
         correct = pred_choice.eq(target.data).cpu().sum()
         print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item()/float(opt.batchSize * 2500)))
 
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
+        if i % 20 == 0:
+            j, data = next(enumerate(valdataloader, 0))
             points, target = data
             points = points.transpose(2, 1)
             points, target = points.cuda(), target.cuda()
@@ -110,7 +143,7 @@ for epoch in range(opt.nepoch):
             loss = F.nll_loss(pred, target)
             pred_choice = pred.data.max(1)[1]
             correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize * 2500)))
+            print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('val'), loss.item(), correct.item()/float(opt.batchSize * 2500)))
 
     torch.save(classifier.state_dict(), '%s/seg_model_%s_%d.pth' % (opt.outf, opt.class_choice, epoch))
 
@@ -140,4 +173,4 @@ for i,data in tqdm(enumerate(testdataloader, 0)):
             part_ious.append(iou)
         shape_ious.append(np.mean(part_ious))
 
-print("mIOU for class {}: {}".format(opt.class_choice, np.mean(shape_ious)))
+print("{} mIOU for class {}: {}".format(blue(test), opt.class_choice, np.mean(shape_ious)))
